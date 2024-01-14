ec164cb1360d05d32d58934d1125d087e580162d
Mourad Azouga <mourad-azouga@outlook.fr>
2024-01-14 22:49:19 +0100

    Retrying 1 1

--- models/base_model.py
+++ models/base_model.py
@@ -3,9 +3,19 @@ from uuid import uuid4
 
 
 class BaseModel:
-    """Parent class BaseModel"""
+    """Parent class for AirBnB clone project
+    Methods:
+        __init__(self, *args, **kwargs)
+        __str__(self)
+        __save(self)
+        __repr__(self)
+        to_dict(self)
+    """
 
     def __init__(self, *args, **kwargs):
+        """
+        Initialize attributes
+        """ 
         date_format = '%Y-%m-%dT%H:%M:%S.%f'
         if kwargs:
             for key, value in kwargs.items():
@@ -23,13 +33,21 @@ class BaseModel:
             self.updated_at = datetime.now()
 
     def __str__(self):
+        """
+        Return class name, id and stuff
+        """
         return '[{}] ({}) {}'.format(self.__class__.__name__, self.id, self.__dict__)
 
     def save(self):
-        """Update current datetime (this is where you'd save to file or database)"""
+        """
+        Update current datetime
+        """
         self.updated_at = datetime.now()
 
     def to_dict(self):
+        """
+        Returns dictionnary
+        """
         dic = self.__dict__.copy()
         dic["created_at"] = self.created_at.isoformat()
         dic["updated_at"] = self.updated_at.isoformat()
@@ -37,4 +55,7 @@ class BaseModel:
         return dic
 
     def __repr__(self):
+        """
+        Return string
+        """
         return self.__str__()
