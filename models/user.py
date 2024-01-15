05ba84a5d20d5144aeed55777c39ed9384453881
Mourad Azouga <mourad-azouga@outlook.fr>
2024-01-15 16:27:15 +0100

    Added user

---
+++ models/user.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Defines the HBnB user."""
+from base_model import BaseModel
+import json
+class user(BaseModel):
+    """
+    This is a user class that has 
+    email-
+    password-
+    first_name
+    last_name
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
